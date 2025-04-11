289b576843da036807e963c743787a9b834fea5c
angie.alcivar.calderon <angie.alcivar.calderon@udla.edu.ec>
2025-04-10 20:36:30 -0500

    :33

--- SE3.c
+++ SE3.c
@@ -3,21 +3,21 @@
 int main() {
     int estudiantes = 5, materias = 3, aprobados = 0, len = 0, val1;
     float calificaciones[5][3], suma = 0;
-    char estudiantes_nombres[5][30];
-    char materias_nombres[3][30] = {"Mate", "Costos", "Progra"};
+    char ests_nombres[5][30];
+    char mats_nombres[3][30] = {"Mate", "Costos", "Progra"};
 
     for (int cont1 = 0; cont1 < 5; cont1++)
     {
         printf("Nombre del estudiante %d: ", cont1 + 1);
         fflush(stdin);
-        fgets(estudiantes_nombres[cont1], 30, stdin);
-        len = strlen(estudiantes_nombres[cont1]) - 1;
-        estudiantes_nombres[cont1][len] = '\0';
+        fgets(ests_nombres[cont1], 30, stdin);
+        len = strlen(ests_nombres[cont1]) - 1;
+        ests_nombres[cont1][len] = '\0';
     }
     for (int cont1 = 0; cont1 < estudiantes; cont1++) {
         for (int cont2 = 0; cont2 < materias; cont2++) {
             do {
-                printf("Calificacion de %s en %s (0-10): ", estudiantes_nombres[cont1], materias_nombres[cont2]);
+                printf("Calificacion de %s en %s (0-10): ", ests_nombres[cont1], mats_nombres[cont2]);
                 fflush(stdin);
                 val1 = scanf("%f", &calificaciones[cont1][cont2]);
                 if (val1 != 1)
@@ -36,7 +36,7 @@ int main() {
     for (int cont1 = 0; cont1 < estudiantes; cont1++) {
         for (int cont2 = 0; cont2 < materias; cont2++)
             suma = suma + calificaciones[cont1][cont2];
-        printf("%s \t\t\t\t  %.2f\t\t\t\n", estudiantes_nombres[cont1], suma / 3);
+        printf("%s \t\t\t\t  %.2f\t\t\t\n", ests_nombres[cont1], suma / 3);
         suma = 0; 
     }
     printf("\n\t   -> PROMEDIOS POR MATERIA <-"); 
@@ -44,7 +44,7 @@ int main() {
     for (int cont2 = 0; cont2 < materias; cont2++) {
         for (int cont1 = 0; cont1 < estudiantes; cont1++)
             suma = suma + calificaciones[cont1][cont2];
-        printf("%s \t\t\t\t\t  %.2f\t\t\t\n", materias_nombres[cont2], suma / 5);
+        printf("%s \t\t\t\t\t  %.2f\t\t\t\n", mats_nombres[cont2], suma / 5);
         suma = 0; 
     }
     printf("\n\t -> NOTA MAS BAJA Y MAS ALTA POR ESTUDIANTE <- \t\n");
@@ -57,7 +57,7 @@ int main() {
             if (calificaciones[cont1][cont2] < min) 
                 min = calificaciones[cont1][cont2];
         }
-        printf("%s\t\t\t%.2f\t\t\t%.2f\n", estudiantes_nombres[cont1], max, min);
+        printf("%s\t\t\t%.2f\t\t\t%.2f\n", ests_nombres[cont1], max, min);
     }
     printf("\n\t -> NOTA MAS BAJA Y MAS ALTA POR MATERIA <- \t\n");
     printf("MATERIA\t\t\t\tMAYOR\t\t\tMENOR\n"); 
@@ -69,7 +69,7 @@ int main() {
             if (calificaciones[cont1][cont2] < min) 
                 min = calificaciones[cont1][cont2];
         }
-        printf("%s\t\t\t\t%.2f\t\t\t%.2f\n", materias_nombres[cont2], max, min);
+        printf("%s\t\t\t\t%.2f\t\t\t%.2f\n", mats_nombres[cont2], max, min);
     }
     printf("\n\t  -> APROBADOS POR MATERIA <-\t");
     printf("\nMATERIA\t\t\t\t\tAPROBADOS\t\n");
@@ -78,7 +78,7 @@ int main() {
             if (calificaciones[cont1][cont2] >= 6)
                 aprobados = aprobados + 1;
         }
-        printf("%s\t\t\t\t\t   %d\n", materias_nombres[cont2], aprobados);
+        printf("%s\t\t\t\t\t   %d\n", mats_nombres[cont2], aprobados);
     }
     return 0;
 }
